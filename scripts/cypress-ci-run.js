f3475334889421cc7ea2e4ebb018edefb601b8af
Mykola <kolianpylupc@gmail.com>
2024-04-03 11:25:35 +0300

    workflow changes

---
+++ scripts/cypress-ci-run.js
@@ -0,0 +1,49 @@
+import { exec } from "child_process";
+
+function getEnvNumber(varName, { required = false } = {}) {
+  if (required && process.env[varName] === undefined) {
+    throw Error(`${varName} is not set.`);
+  }
+
+  const value = Number(process.env[varName]);
+
+  if (isNaN(value)) {
+    throw Error(`${varName} is not a number.`);
+  }
+
+  return value;
+}
+
+function getArgs() {
+  return {
+    totalRunners: getEnvNumber("TOTAL_RUNNERS", { required: true }),
+    thisRunner: getEnvNumber("THIS_RUNNER", { required: true }),
+  };
+}
+
+(async () => {
+  try {
+    const { totalRunners, thisRunner } = getArgs();
+
+    const command = `yarn cypress run --spec "$(yarn --silent ts-node --quiet scripts/cypress-spec-split.ts ${totalRunners} ${thisRunner})"`;
+
+    console.log(`Running: ${command}`);
+
+    const commandProcess = exec(command);
+
+    if (commandProcess.stdout) {
+      commandProcess.stdout.pipe(process.stdout);
+    }
+
+    if (commandProcess.stderr) {
+      commandProcess.stderr.pipe(process.stderr);
+    }
+
+    commandProcess.on("exit", (code) => {
+      process.exit(code || 0);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
---
+++ scripts/cypress-spec-split.js
@@ -0,0 +1,107 @@
+import fs from 'fs/promises';
+import globby from 'globby';
+import minimatch from 'minimatch';
+
+// These are the same properties that are set in cypress.config.
+// In practice, it's better to export these from another file, and
+// import them here and in cypress.config, so that both files use
+// the same values.
+const specPatterns = {
+  specPattern: 'cypress/e2e/**/*.cy.{ts,tsx,js,jsx}',
+};
+
+// used to roughly determine how many tests are in a file
+const testPattern = /(^|\s)(it|test)\(/g;
+
+const isCli = true;
+
+function getArgs() {
+  const [totalRunnersStr, thisRunnerStr] = process.argv.splice(2);
+
+  if (!totalRunnersStr || !thisRunnerStr) {
+    throw new Error('Missing arguments');
+  }
+
+  const totalRunners = totalRunnersStr ? Number(totalRunnersStr) : 0;
+  const thisRunner = thisRunnerStr ? Number(thisRunnerStr) : 0;
+
+  if (isNaN(totalRunners)) {
+    throw new Error('Invalid total runners.');
+  }
+
+  if (isNaN(thisRunner)) {
+    throw new Error('Invalid runner.');
+  }
+
+  return { totalRunners, thisRunner };
+}
+
+async function getTestCount(filePath) {
+  const content = await fs.readFile(filePath, 'utf8');
+  return (content.match(testPattern) || []).length || 0;
+}
+
+async function getSpecFilePaths() {
+  const options = specPatterns;
+
+  const files = await globby(options.specPattern);
+
+  const ignorePatterns = [...(options.excludeSpecPattern || [])];
+
+  const doesNotMatchAllIgnoredPatterns = (file) => {
+    const MINIMATCH_OPTIONS = { dot: true, matchBase: true };
+    return ignorePatterns.every((pattern) => {
+      return !minimatch(file, pattern, MINIMATCH_OPTIONS);
+    });
+  };
+
+  const filtered = files.filter(doesNotMatchAllIgnoredPatterns);
+  return filtered;
+}
+
+async function sortSpecFilesByTestCount(specPathsOriginal) {
+  const specPaths = [...specPathsOriginal];
+  const testPerSpec = {};
+
+  for (const specPath of specPaths) {
+    testPerSpec[specPath] = await getTestCount(specPath);
+  }
+
+  return Object.entries(testPerSpec)
+    .sort((a, b) => b[1] - a[1])
+    .map((x) => x[0]);
+}
+
+function splitSpecs(specs, totalRunners, thisRunner) {
+  return specs.filter((_, index) => index % totalRunners === thisRunner);
+}
+
+(async () => {
+  if (!isCli) {
+    return;
+  }
+
+  try {
+    const specFilePaths = await sortSpecFilesByTestCount(
+      await getSpecFilePaths(),
+    );
+
+    console.log('specFilePaths', specFilePaths);
+
+    if (!specFilePaths.length) {
+      throw Error('No spec files found.');
+    }
+
+    const { totalRunners, thisRunner } = getArgs();
+
+    console.log('totalRunners', totalRunners);
+    console.log('thisRunner', thisRunner);
+
+    const specsToRun = splitSpecs(specFilePaths, totalRunners, thisRunner);
+
+    console.log(specsToRun.join(','));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();

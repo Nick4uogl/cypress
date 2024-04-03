1710528dbc1f83ce34e76854d9124fe363ab8996
Mykola <kolianpylupc@gmail.com>
2024-04-03 11:30:47 +0300

    changes to test runner script

--- scripts/cypress-ci-run.js
+++ scripts/cypress-ci-run.js
@@ -25,7 +25,7 @@ function getArgs() {
   try {
     const { totalRunners, thisRunner } = getArgs();
 
-    const command = `yarn cypress run --spec "$(yarn --silent ts-node --quiet scripts/cypress-spec-split.ts ${totalRunners} ${thisRunner})"`;
+    const command = `npx cypress run --spec "$(node scripts/cypress-spec-split.ts ${totalRunners} ${thisRunner})"`;
 
     console.log(`Running: ${command}`);
 
